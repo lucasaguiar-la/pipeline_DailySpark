2ef50e8e8589714da4b492aec851ea3b49c552d0
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-02-19 16:12:18 -0300

    feat: creation of the function to get crawl segments

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from scripts.crawl_info import get_crawl_info
+from scripts.crawl_info import get_crawl_indexes, get_crawl_segments
 
 if __name__ == '__main__':
-    crawls = get_crawl_info()
+    crawls = get_crawl_indexes()
     if crawls:
-        print(f'crawls disponíveis ({len(crawls)}):')
-        for idx, crawl in enumerate(crawls[:3]):
+        print(f'Crawls disponíveis ({len(crawls)}):')
+        for idx, crawl in enumerate(crawls[:5]):
             print(f'{idx + 1} - {crawl['name']}')
+        
+        option = int(input('\nEscolha o número do crawl que deseja explorar: ')) -1
+        selected_option = crawls[option]
+
+        print(
+            f'\nExplorando crawl: {selected_option['name']}'
+            f'ID: {selected_option['id']}'
+            )
+        
+        print('\nBuscando segmentos...')
+        segments = get_crawl_segments(crawl_id=selected_option['id'])
+        if segments:
+            for segment in segments[:5]:
+                print(segments)
--- scripts/crawl_info.py
+++ scripts/crawl_info.py
@@ -1,15 +1,36 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-def get_crawl_info():
+load_dotenv()
+
+def get_crawl_indexes():
+    index_url = os.getenv('INDEX_URL')
+
     try:
-        index_url = 'https://index.commoncrawl.org/collinfo.json'
         response = requests.get(index_url)
         response.raise_for_status()
         data = response.json()
+
     except requests.exceptions.HTTPError as e:
         print(f'Algo deu errado: {e}')
         return None
-    except Exception as e:
-        print(f'Outro erro ocorreu: {e}')
-        return None
+
     return data
+
+def get_crawl_segments(crawl_id):
+    base_url = os.getenv('BASE_URL')
+    segments_url = (f'{base_url}{crawl_id}-index')
+    params = {
+        'url': 'example.com', 
+        'output': 'json'
+    }
+    try:
+        response = requests.get(segments_url, params=params)
+        response.raise_for_status()
+
+    except requests.exceptions.HTTPError as e:
+        print(f'Algo deu errado: {e}')
+        return None
+
+    return response.text.splitlines()
